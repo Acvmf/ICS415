e22873ef62e6aab473de33686219c6a937e877a0
Lexa <acvmf@hawaii.edu>
2015-09-20 21:54:22 -1000

    Week 4_5
    fixed where the bits should go

---
+++ Homework/A07/myScript.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	function getClasses(elem){
+		var classes = elem.className;
+		return classes;
+	}
+	function addClass (elem, className) {
+		var d =document.getElementById('elem');
+		d.classList.add("className");
+	}
+</script>
