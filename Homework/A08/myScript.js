73ab4bf4ae2fac03a38cb4d3fe195749ec24fb88
Lexa <acvmf@hawaii.edu>
2015-09-27 19:24:37 -1000

    A08 upload 2

--- Homework/A08/myScript.js
+++ Homework/A08/myScript.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
 	$(".answer").hide();
-	$("#one").click(function(){
-		$("#1").toggle();
-		$("#one").
-	});
+
+	$("#one").click(
+		function(){
+			$("#1").toggle(400,document.getElementById('one').innerHTML="(+) 1. Hi, I'm {Name}.")
+		}
+	);
+	
 	$("#two").click(function(){
 		$("#2").toggle();
+		document.getElementById('two').innerHTML="(+) 2. So are you a student?";
 	});
+	
 	$("#three").click(function(){
 		$("#3").toggle();
+		document.getElementById('three').innerHTML="(+) 3. Wow, you must be crazy smart!";
 	});
+	
 	$("#four").click(function(){
 		$("#4").toggle();
+		document.getElementById('four').innerHTML="(+) 4. So what are your plans for the future?";
 	});
+	
 	$("#five").click(function(){
 		$("#5").toggle();
+		document.getElementById('five').innerHTML="(+) 5. Isn't that impossible?";
 	});
 });
